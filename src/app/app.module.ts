982f19c692839389f31cdf604a7b8cad86fd6df4
Melissa Al'nair <melissa.alnair@gmail.com>
2021-05-31 21:12:43 -0300

    Subindo projeto no github

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { registerLocaleData } from "@angular/common";
+
+import {LOCALE_ID, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { DashboardModule } from "./dashboard/dashboard.module";
+import { SharedModule } from "./shared/shared.module";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import localePt from "@angular/common/locales/pt";
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DashboardModule,
+    SharedModule,
+  ],
+  providers: [ 
+    {provide: LOCALE_ID, useValue: 'pt-BR'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,12 @@
+class Stock {
+    id: number;
+    name: string;
+    price: number;
+    minimun: number;
+    exclusiveMinimum: boolean;
+    variation: number;
+    date: string;
+
+}
+
+export default Stock;
